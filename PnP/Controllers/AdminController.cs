9f9de4a352fe2eb89b0ff241c702fdf9ac5907ca
jojomils <50737155+jojomils@users.noreply.github.com>
2019-05-27 16:05:38 +0200

    Update AdminController.cs

--- PnP/Controllers/AdminController.cs
+++ PnP/Controllers/AdminController.cs
@@ -1,16 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PnP.Models;
 using System.Linq;
 
 namespace PnP.Controllers {
 
+  [Authorize]
   public class AdminController : Controller
   {
     private IProductRepository repository;
 
-    public AdminController(IProductRepository repo)
+    private IOrderRepository reps;
+
+    public AdminController(IProductRepository repo, IOrderRepository rep)
     {
       repository = repo;
+      reps = rep;
     }
 
     public ViewResult Index() => View(repository.Products);
@@ -35,6 +40,30 @@ namespace PnP.Controllers {
       }
     }
 
+    //This is to call the order controller List to view it.
+    public ViewResult Unshipped() =>
+       View(reps.Orders.Where(o => !o.Shipped));
+
+    //This is to call the Shipped View to view all shipped orders
+    public ViewResult Shipped() => View(reps.Orders.Where(s => s.Shipped));
+
+    //This is to call the unshipped view to see all unshipped orders
+    public ViewResult List() => View(reps.Orders);
+
+    [HttpPost]
+    public IActionResult MarkShipped(int orderID)
+    {
+      Order order = reps.Orders
+          .FirstOrDefault(o => o.OrderID == orderID);
+      if (order != null)
+      {
+        order.Shipped = true;
+        reps.SaveOrder(order);
+      }
+      return RedirectToAction(nameof(List));
+    }
+
+    //Creates a new product
     public ViewResult Create() => View("Edit", new Product());
 
     [HttpPost]
