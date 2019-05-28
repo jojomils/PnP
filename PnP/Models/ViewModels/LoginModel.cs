fdb1a32c7c045c039668032627f78646e3e2d14c
jojomils <50737155+jojomils@users.noreply.github.com>
2019-05-28 08:03:46 +0200

    Add files via upload

---
+++ PnP/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PnP.Models.ViewModels
+{
+  public class LoginModel
+  {
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    [UIHint("Password")]
+    public string Password { get; set; }
+
+    public String ReturnUrl { get; set; } = "/";
+  }
+}
